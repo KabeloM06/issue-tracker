c7b357851fbad201aafd7bdf07743cbc8a730744
KabeloM06 <69459421+KabeloM06@users.noreply.github.com>
2023-10-23 10:01:19 +0200

    Add new issue

--- src/app/issue-report/issue-report.component.ts
+++ src/app/issue-report/issue-report.component.ts
@@ -1,17 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 import {FormBuilder, FormGroup} from '@angular/forms';
+import { IssuesService } from '../issues.service';
 
 @Component({
   selector: 'app-issue-report',
   templateUrl: './issue-report.component.html',
   styleUrls: ['./issue-report.component.css']
 })
+
+
+
 export class IssueReportComponent implements OnInit {
 
   issueForm: FormGroup | undefined;
 
-  constructor(private builder: FormBuilder) { }
+  @Output() formClose = new EventEmitter();
+
+  constructor(private builder: FormBuilder,
+              private issueService: IssuesService) { }
 
   ngOnInit(): void {
     this.issueForm = this.builder.group({
@@ -20,6 +27,11 @@ export class IssueReportComponent implements OnInit {
       priority: [''],
       type:['']
     });
+  }
+
+  addIssue(){
+    this.issueService.createIssue(this.issueForm?.value);
+    this.formClose.emit();
   }
 
 }
